bfe0e163c7f0521e1e9dc1a7a1a2140ff27f9c24
otelooh-prog <otelooh@gmail.com>
2026-02-23 16:15:13 +0100

    Update meross_test.py

--- meross_test.py
+++ meross_test.py
@@ -4,10 +4,9 @@ import asyncio
 
 EMAIL = "jose.alconchel@gmx.com"
 PASSWORD = "pa1pe2pi3"
-DEVICE_NAME = "Marantz M-CR610"   # Canvia-ho si cal
+DEVICE_NAME = "Marantz M-CR610"   # Canvia-ho després
 
 async def prova_endoll():
-    # Connexió al Cloud de Meross
     http_client = await MerossHttpClient.async_from_user_password(
         email=EMAIL,
         password=PASSWORD,
@@ -17,14 +16,12 @@ async def prova_endoll():
     manager = MerossManager(http_client=http_client)
     await manager.async_init()
 
-    # Obtenir tots els dispositius del compte
     devices = manager.find_devices()
 
     # MOSTRAR ELS NOMS REALS QUE VEU LA API
     for d in devices:
         print("TROBAT:", d.name)
 
-    # Buscar el dispositiu pel nom
     target = None
     for d in devices:
         if d.name == DEVICE_NAME:
@@ -35,16 +32,13 @@ async def prova_endoll():
         print("Dispositiu no trobat")
         return
 
-    # Encendre i apagar per provar
     await target.async_turn_off()
     print("OFF")
     await asyncio.sleep(10)
     await target.async_turn_on()
     print("ON")
 
-    # Tancar connexió
     await manager.async_close()
     await http_client.async_logout()
 
-# Executar la funció
 asyncio.run(prova_endoll())
